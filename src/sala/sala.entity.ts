db9b8ea18d5455063243c39643cff9ea5633a8e1
Henrique Rosa <rosa.henrique@outlook.com.br>
2021-10-29 18:42:39 -0300

    removido arquivos não usados

--- src/sala/sala.entity.ts
+++ src/sala/sala.entity.ts
@@ -2,35 +2,33 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity('sala')
 export class SalaEntity {
-  @PrimaryGeneratedColumn() 
+  @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ name: 'nome'}) 
+  @Column({ name: 'nome' })
   nome: string;
 
-  @Column({ name: 'inicio_intervalo'}) 
+  @Column({ name: 'inicio_intervalo' })
   intervalo_inicio: string; // atenção!!!!!!!
-  
-  @Column({ name: 'status'}) 
+
+  @Column({ name: 'status' })
   status: boolean;
 
-  @Column({ name: 'predio_id'}) 
+  @Column({ name: 'predio_id' })
   predio_id: number;
 
-  @Column({ name: 'fim_intervalo'}) 
+  @Column({ name: 'fim_intervalo' })
   intervalo_fim: string;
 
-  @Column({ name: 'equipamentos'}) 
+  @Column({ name: 'equipamentos' })
   equipamentos: string;
 
-  @Column({ name: 'salas_especiais'}) 
+  @Column({ name: 'salas_especiais' })
   salas_especiais: string;
 
-  @Column({ name: 'andar'}) 
+  @Column({ name: 'andar' })
   andar: number;
 
-  @Column({ name: 'capacidade'}) 
+  @Column({ name: 'capacidade' })
   capacidade: number;
-
-  
 }
--- src/sala/sala.validation.ts
+++ src/sala/sala.validation.ts
@@ -1,44 +1,37 @@
 import * as Yup from 'yup';
 
 export const salaValidation = Yup.object({
-    nome: Yup.string()
-        .required('Campo obrigatório')
-        .max(50, 'Máximo de 50 caracteres'),
+  nome: Yup.string()
+    .required('Campo obrigatório')
+    .max(50, 'Máximo de 50 caracteres'),
 
-    equipamentos: Yup.string()
-        .required('Campo Obrigatório')
-        .max(500, 'Máximo de 500 caracteres'),
+  equipamentos: Yup.string()
+    .required('Campo Obrigatório')
+    .max(500, 'Máximo de 500 caracteres'),
 
-    andar: Yup.number()
-        .required('Campo obrigatório')
-        .max(9, 'Valor maximo 9')
-        .min(-9, 'Valor minimo -9'),
+  andar: Yup.number()
+    .required('Campo obrigatório')
+    .max(9, 'Valor maximo 9')
+    .min(-9, 'Valor minimo -9'),
 
-    intervalo_fim: Yup.string()
-        .required('Campo obrigatório')
-        .max(5, 'Máximo de 5 caracteres'),
+  intervalo_fim: Yup.string()
+    .required('Campo obrigatório')
+    .max(5, 'Máximo de 5 caracteres'),
 
-    intervalo_inicio: Yup.string()
-        .required('Campo obrigatório')
-        .max(5, 'Máximo de 5 caracteres'),
+  intervalo_inicio: Yup.string()
+    .required('Campo obrigatório')
+    .max(5, 'Máximo de 5 caracteres'),
 
-    status: Yup.boolean()
-        .required('Campo obrigatório'),
+  predio_id: Yup.number().required('Campo obrigatório').positive('Id invalido'),
 
-    predio_id: Yup.number()
-        .required('Campo obrigatório')
-        .positive('Id invalido'),
+  salas_especiais: Yup.string().max(150, 'Máximo de 150 caracteres'),
 
-    salas_especiais: Yup.string()
-        .max(150, 'Máximo de 150 caracteres'),
-
-    capacidade: Yup.number()
-        .required('Campo obrigatório')
-        .positive('Somente valores positivos')
-        .max(999, 'Máximo de 3 dígitos'),
+  capacidade: Yup.number()
+    .required('Campo obrigatório')
+    .positive('Somente valores positivos')
+    .max(999, 'Máximo de 3 dígitos'),
 });
 
 export const altera_statusValidation = Yup.object({
-    status: Yup.boolean()
-    .required('Campo obrigatório'),
+  status: Yup.boolean().required('Campo obrigatório'),
 });
