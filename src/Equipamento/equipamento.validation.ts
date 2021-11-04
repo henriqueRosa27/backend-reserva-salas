08673212fbecca55ef3e3f59206798be6465292d
yuri <atzleryuri@gmail.com>
2021-11-04 19:20:37 -0300

    verificação predios e salas

--- src/Equipamento/equipamento.validation.ts
+++ src/Equipamento/equipamento.validation.ts
@@ -1,18 +1,18 @@
 import * as Yup from 'yup';
 
 export const EquipamentoValidation = Yup.object({
-    nome_equipamentos: Yup.string()
-        .required('Campo nome obrigatório')
-        .max(30, 'Máximo de 30 caracteres no campo nome'),
+  nome_equipamentos: Yup.string()
+    .required('Campo nome obrigatório')
+    .max(30, 'Máximo de 30 caracteres no campo nome'),
 
-    quantidade: Yup.number()
-        .required('Campo quantiade obrigatório')
-        .max(99, 'Máximo e 99 equipamentos no campo quantidade')
-        .positive('Somente valores positivos no campo quantidade'),
-        
-    });
+  quantidade: Yup.number()
+    .required('Campo quantiade obrigatório')
+    .max(99, 'Máximo e 99 equipamentos no campo quantidade')
+    .positive('Somente valores positivos no campo quantidade'),
 
-    export const altera_statusValidation = Yup.object({
-        status: Yup.boolean()
-        .required('Campo status obrigatório'),
-    })
+  status: Yup.boolean().required('Campo status obrigatório'),
+});
+
+export const altera_statusValidation = Yup.object({
+  status: Yup.boolean().required('Campo status obrigatório'),
+});
